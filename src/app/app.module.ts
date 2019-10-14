bd559249122b84a6fc2aa17841735115a473d58f
Luca Quitadamo <lucaquitadamo@mac.speedport_w_724v_typ_a_05011603_06_001>
2019-10-14 12:44:29 +0200

    Raum-Such-Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { MatRadioModule, MatCheckboxModule, MatDatepickerModule, MatNativeDateModule, MatFormFieldModule, MatInputModule, MAT_DATE_LOCALE } from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { RegisterFormComponent } from './register-form/register-form.component';
 import { RoomCardComponent } from './room-card/room-card.component';
 import { RoomListComponent } from './room-list/room-list.component';
-import {HttpClientModule} from '@angular/common/http';
 import { RoomDetailComponent } from './room-detail/room-detail.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
 
 const appRoutes: Routes = [
   { path: '', component: AppComponent },
   { path: 'login', component: LoginFormComponent },
   { path: 'register', component: RegisterFormComponent },
+  { path: 'room/:id', component: RoomDetailComponent },
   { path: 'rooms', component: RoomListComponent },
-  { path: 'room/:id', component: RoomDetailComponent }
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
@@ -25,16 +32,27 @@ const appRoutes: Routes = [
     RegisterFormComponent,
     RoomCardComponent,
     RoomListComponent,
-    RoomDetailComponent
-  ],
+    RoomDetailComponent,
+    NotFoundComponent
+    ],
   imports: [
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot(
       appRoutes
-    )
+    ),
+    FormsModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'de-DE'},
+  ],
   bootstrap: [AppComponent, LoginFormComponent, RegisterFormComponent, RoomDetailComponent]
 })
 export class AppModule { }
